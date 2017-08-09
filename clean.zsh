589e4e38e7800d7fa0ccbc69e454f05178c7c151
Brandon Roehl <BrandonRoehl@gmail.com>
2017-08-08 19:11:07 -0500

    init for the prompt

--- clean.zsh
+++ clean.zsh
@@ -2,7 +2,7 @@
 # turns seconds into human readable time
 # 165392 => 1d 21h 56m 32s
 # https://github.com/sindresorhus/pretty-time-zsh
-function clean_prompt_human_time_to_var() {
+prompt_human_time_to_var() {
 	local human total_seconds=$1 var=$2
 	local days=$(( total_seconds / 60 / 60 / 24 ))
 	local hours=$(( total_seconds / 60 / 60 % 24 ))
@@ -17,23 +17,52 @@ function clean_prompt_human_time_to_var() {
 	print "${human}"
 }
 
-function clean_prompt_init() {
+# stores (into prompt_pure_cmd_exec_time) the exec time of the last command if set threshold was exceeded
+prompt_check_cmd_exec_time() {
+	integer elapsed
+	(( elapsed = EPOCHSECONDS - ${cmd_timestamp:-$EPOCHSECONDS} ))
+	prompt_pure_cmd_exec_time=
+	(( elapsed > ${CMD_MAX_EXEC_TIME:=5} )) && {
+		prompt_pure_human_time_to_var $elapsed "prompt_pure_cmd_exec_time"
+	}
+}
+
+prompt_chwd() {
+    (git fetch &)
+}
+
+prompt_preexec() {
+    cmd_timestamp=$EPOCHSECONDS
+}
+
+prompt_precmd() {
+    vcs_info
+    cmd_exec_time=`prompt_check_cmd_exec_time`
+    unset prompt_cmd_timestamp
+}
+
+prompt_init() {
+	setopt localoptions noshwordsplit
     # Set required options
     setopt prompt_subst
 
     # Load required modules
     autoload -Uz vcs_info
 
-
     zstyle ':vcs_info:*' enable hg bzr git
-    zstyle ':vcs_info:*:*' unstagedstr '*'
-    zstyle ':vcs_info:*:*' stagedstr '+'
+    zstyle ':vcs_info:*' unstagedstr '*'
+    zstyle ':vcs_info:*' stagedstr '+'
     zstyle ':vcs_info:*:*' formats "%s/%b" "%u%c"
     zstyle ':vcs_info:*:*' actionformats "%s/%b" "%u%c" "(%a)"
     zstyle ':vcs_info:git:*' formats "%b" "%u%c"
     zstyle ':vcs_info:git:*' actionformats "%b" "%u%c" "(%a)"
-    zstyle ':vcs_info:*:*' nvcsformats "%~" ""
 
 
+
+    add-zsh-hook precmd prompt_precmd
+	add-zsh-hook preexec prompt_preexec
 }
+
+
+
 
