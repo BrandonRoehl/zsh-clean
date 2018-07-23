5e0a102ed224994562794b914edf53c204e84e66
Brandon Roehl <me@brandonroehl.org>
2018-07-23 06:07:28 -0500

    Move term to inside check

--- clean.plugin.zsh
+++ clean.plugin.zsh
@@ -2,9 +2,5 @@
 autoload -U promptinit
 promptinit
 # Check if the terminal supports 256 bit color
-if [[ $TERM =~ '256color$' ]]; then
-    prompt clean 256
-else
-    prompt clean
-fi
+prompt clean $TERM
 
