753d846336c7abc4a6941b5fd265286a65690ad7
Brandon Roehl <SkyGuy913@gmail.com>
2018-08-01 21:06:24 -0500

    Add the folder to the fpath if it doesn't exist in it

--- clean.plugin.zsh
+++ clean.plugin.zsh
@@ -1,6 +1,14 @@
+# Add this folder to the fpath if it is not
+function clean_plugin_zsh() {
+    local folder="${0:A:h}"
+    if [[ -z "${fpath[(r)$folder]}" ]]; then
+        fpath+=( folder )
+    fi
+}
+clean_plugin_zsh()
 # Legacy support for those already using antigen
 autoload -Uz promptinit
 promptinit
 # Check if the terminal supports 256 bit color
-prompt clean $TERM
+prompt clean
 
